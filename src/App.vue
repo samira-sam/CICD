16f35d3f9a93087f00b57316706425cf1976b024
samira-sam <samyrra@hotmail.fr>
2025-03-10 10:41:05 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="coucou"/>
+  </div>
 </template>
 
-<script>
+<script setup>
 import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-};
+
+
 </script>
 
 <style>
---
+++ tests/unit/HelloWorld.spec.js
@@ -0,0 +1,14 @@
+import { mount } from '@vue/test-utils';
+import HelloWorld from '../../src/components/HelloWorld.vue';
+
+describe('HelloWorld.vue', () => {
+  it('afficher le msg', () => {
+    const wrapper = mount(HelloWorld, {
+      props: {
+        msg: 'salut',
+      },
+    });
+
+    expect(wrapper.text()).toContain('salut');
+  });
+});
